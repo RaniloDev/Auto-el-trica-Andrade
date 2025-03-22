26bfc24862e1b82708a5dae28bdf8abd06d8c022
Ranilo <12ranilo3@gmail.com>
2025-03-22 18:54:54 -0300

    feat: add npm run

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/',
+        permanent: false,
+      },
+    ]
+  },
+}
